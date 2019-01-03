429505767e52e4d58e49b0127ace3c3b08606c91
zhangzhongxiang <zhangzhongxiang@100tal.com>
2019-01-03 19:12:05 +0800

    fix

--- lisp/init-edit-utils.el
+++ lisp/init-edit-utils.el
@@ -5,7 +5,6 @@
 ;; smartparens
 (require-package 'smartparens)
 (require 'smartparens-config)
-(smartparens-global-mode t)
 
 ;; mark active key binding
 (global-set-key (kbd "C-x C-x") 'set-mark-command)
