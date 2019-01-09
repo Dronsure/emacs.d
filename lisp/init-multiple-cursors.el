cb93a91a37d99ee8732c47d2ca26c2fddd9c8271
zhangzhongxiang <zhangzhongxiang@100tal.com>
2019-01-09 18:10:44 +0800

    update

--- lisp/init-multiple-cursors.el
+++ lisp/init-multiple-cursors.el
@@ -1,6 +1,6 @@
 (require-package 'multiple-cursors)
 
-(global-set-key (kbd "C-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
 
 (provide 'init-multiple-cursors)
